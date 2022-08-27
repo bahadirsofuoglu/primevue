a275a6f1ff33671b72eee93dc4aaafe5b8c45ec8
Bahadir Sofuoglu <bahadirsofuoglu@gmail.com>
2022-08-27 14:20:32 +0300

    Prettier cofiguration completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    jest: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    root: true,
+    env: {
+        node: true,
+        jest: true
+    },
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'prettier'],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'vue/attributes-order': [
+            'error',
+            {
+                order: ['DEFINITION', 'LIST_RENDERING', 'CONDITIONALS', 'RENDER_MODIFIERS', 'GLOBAL', ['UNIQUE', 'SLOT'], 'TWO_WAY_BINDING', 'OTHER_DIRECTIVES', 'OTHER_ATTR', 'EVENTS', 'CONTENT'],
+                alphabetical: false
+            }
+        ]
+    }
+};
